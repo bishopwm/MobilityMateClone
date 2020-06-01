a42cee6cd742c8948e8f59ad54aacfa7db943273
bishopwm <bishopwm@g.cofc.edu>
2020-06-01 18:29:09 -0400

    undo last commit

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -10,19 +10,19 @@ const service = axios.create({ withCredentials: true, baseURL });
 
 const actions = {
   isLoggedIn: async () => {
-    return await service.get('/is-logged-in/')
+    return await service.get('/is-logged-in')
   },
   signUp: async (user) => {
-    return await service.post('/signup/', user)
+    return await service.post('/signup', user)
   },
   logIn: async (user) => {
-    return await service.post('/login/', user)
+    return await service.post('/login', user)
   },
   logOut: async () => {
-    return await service.get('/logout/')
+    return await service.get('/logout')
   },
   returnMobilityData: async (regionName) => {
-    return await service.get('/mobilities/', {params: 
+    return await service.get('/mobilities', {params: 
       { region: regionName }
     })
   },
@@ -30,7 +30,7 @@ const actions = {
     return await service.post('/saved-grocery-data/', user)
   },
   updateUserParksData: async (user) => {
-    return await service.post('/saved-parks-data/', user)
+    return await service.post('/saved-parks-data', user)
   }
 };
 
